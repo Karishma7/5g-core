d052ea954b42ee411203f931727825f4b5d69671
Karishma7 <76575479+Karishma7@users.noreply.github.com>
2021-10-07 18:41:22 +0530

    Update build_docker_images.sh

--- build_docker_images.sh
+++ build_docker_images.sh
@@ -24,7 +24,7 @@ if [ -z `which docker` ]; then
     sudo apt-get install -y docker.io
 fi
 if [ -z `which docker-compose` ]; then
-    sudo curl -L "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    sudo curl -L "https://github.com/docker/compose/releases/download/17.06.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
     sudo chmod +x /usr/local/bin/docker-compose
 fi
 dir=dockerfiles
